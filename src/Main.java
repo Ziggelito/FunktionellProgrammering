a6357331012eebc0a60ed8bcd111a289ab683931
Ziggelito <Sixten.Lonnberg@yh.nackademin.se>
2023-02-16 20:17:40 +0100

    ja

--- src/Main.java
+++ src/Main.java
@@ -4,10 +4,7 @@ import java.sql.CallableStatement;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.util.InputMismatchException;
-import java.util.List;
-import java.util.Properties;
-import java.util.Scanner;
+import java.util.*;
 
 
 public class Main  {
@@ -125,7 +122,9 @@ public class Main  {
 
 
 
-        int answer = inputInt("Vilken sko vill du beställa?:" +
+
+
+        int answer = inputInt ("Vilken sko vill du beställa?:" +
                 "\n1. Storlek: " + productsList.get(0).getSize() + " Färg: " + productsList.get(0).getColor() +  " Pris: " + productsList.get(0).getPrice() + "Kr " + productsList.get(0).getBrand() + " Lager: " + productsList.get(0).getStock() +
                 "\n2. Storlek: " + productsList.get(1).getSize() + " Färg: " + productsList.get(1).getColor() +  " Pris: " + productsList.get(1).getPrice() + "Kr " + productsList.get(1).getBrand() + " Lager: " + productsList.get(1).getStock() +
                 "\n3. Storlek: " + productsList.get(2).getSize() + " Färg: " + productsList.get(2).getColor() +  " Pris: " + productsList.get(2).getPrice() + "Kr " + productsList.get(2).getBrand() + " Lager: " + productsList.get(2).getStock() +
@@ -135,6 +134,8 @@ public class Main  {
                 "\n6. Storlek: " + productsList.get(5).getSize() + " Färg: " + productsList.get(5).getColor() +  " Pris: " + productsList.get(5).getPrice() + "Kr " + productsList.get(5).getBrand() + " Lager: " + productsList.get(5).getStock() +
                 "\n7. Storlek: " + productsList.get(6).getSize() + " Färg: " + productsList.get(6).getColor() +  " Pris: " + productsList.get(6).getPrice() + "Kr " + productsList.get(6).getBrand() + " Lager: " + productsList.get(6).getStock() +
                 "\n8. Storlek: " + productsList.get(7).getSize() + " Färg: " + productsList.get(7).getColor() +  " Pris: " + productsList.get(7).getPrice() + "Kr " + productsList.get(7).getBrand() + " Lager: " + productsList.get(7).getStock()  );
+        productsList.stream().sorted(Comparator.comparingInt(Products::getProductID)).forEach(s -> System.out.println(s.getBrand()+"\t"
+                +s.getColor()+" \t"+ s.getSize()+"\t\t\t"+s.getPrice()+" \t"+s.getStock()));
 
 
         switch (answer){
